44818a176455af670207afd5e3f27c08e7a4fd06
DESKTOP-8M44C05\Vladimir Makarevich <makarevich.dev@outlook.com>
2020-12-19 19:03:52 +0300

    feat: work on the styling of omnifood page

--- src/app/modules/natours/natours.component.ts
+++ src/app/modules/natours/natours.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-natours',
   templateUrl: './natours.component.html',
-  styleUrls: ['./natours.component.scss']
+  styleUrls: ['./natours.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
 })
 export class NatoursComponent {
 }
--- src/app/modules/omnifood/omnifood.component.ts
+++ src/app/modules/omnifood/omnifood.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-omnifood',
   templateUrl: './omnifood.component.html',
-  styleUrls: ['./omnifood.component.scss']
+  styleUrls: ['./omnifood.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
 })
 export class OmnifoodComponent {
 }
